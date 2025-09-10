591282a603819829bd5a83181e4bf8dd33db67b9
aoli-al <aoli.al@hotmail.com>
2025-09-10 12:19:11 -0400

    update

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -16,6 +16,7 @@
       set -g pane-base-index 1
       set -g renumber-windows on
       set -s escape-time 0
+      set-option -g default-shell ${pkgs.fish}/bin/fish
       
       # Visual mode key bindings
       bind-key -T copy-mode-vi v send-keys -X begin-selection
