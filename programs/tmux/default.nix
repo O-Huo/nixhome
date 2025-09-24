6431628fdff24c40ee85811bde24a93d8c8e8b3d
aoli-al <aoli.al@hotmail.com>
2025-09-23 20:09:30 -0400

    update default-command in tmux as well

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -17,6 +17,7 @@
       set -g renumber-windows on
       set -s escape-time 0
       set-option -g default-shell ${pkgs.fish}/bin/fish
+      set-option -g default-command ${pkgs.fish}/bin/fish
       
       # Visual mode key bindings
       bind-key -T copy-mode-vi v send-keys -X begin-selection
