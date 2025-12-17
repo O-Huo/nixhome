551ba11d896b5113f6f8cad5dd320fa732cb13b2
aoli-al <aoli.al@hotmail.com>
2025-12-17 15:25:43 -0500

    update

--- programs/hosts/jex.nix
+++ programs/hosts/jex.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   wayland.windowManager.hyprland.extraConfig = "
-    monitor=eDP-1, 1920x1200@60, 0x0, 1.25
+    monitor=eDP-1, 1920x1200@60, 0x0, 1
     ";
   
   services.hypridle.settings.listener = [
@@ -9,4 +9,26 @@
       on-timeout = "systemctl suspend";
     }
   ];
+  programs.niri.settings = {
+    cursor = {
+        theme = "Nordic-cursors";
+        size = 48;
+      };
+    outputs."eDP-1" = {
+      scale = 1;
+      mode = {
+        width = 1920;
+        height = 1200;
+        refresh = 59.950;
+      };
+    };
+    outputs."ASUSTek COMPUTER INC PG32UCDM S3LMQS114886" = {
+      scale = 1.5;
+      mode = {
+        width = 3840;
+        height = 2160;
+        refresh = 119.880;
+      };
+    };
+  };
 }
