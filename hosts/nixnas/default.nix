ed074a94889fc595b293b8f4522b6025e47ff796
XiangpengHao <me@xiangpeng.systems>
2026-01-24 12:22:26 -0600

    update

--- hosts/nixnas/default.nix
+++ hosts/nixnas/default.nix
@@ -6,43 +6,10 @@
     })
     ../common/hao.nix
     ./hardware-configuration.nix
-    (modulesPath + "/image/repart.nix")
   ];
 
   networking.hostName = "nixnas";
 
   # VM-specific settings
   services.qemuGuest.enable = true;
-
-  # Image generation settings for TrueNAS
-  image.repart = {
-    name = "nixnas";
-    partitions = {
-      "esp" = {
-        contents = {
-          "/EFI/BOOT/BOOTX64.EFI".source = "${pkgs.systemd}/lib/systemd/boot/efi/systemd-bootx64.efi";
-          "/EFI/systemd/systemd-bootx64.efi".source = "${pkgs.systemd}/lib/systemd/boot/efi/systemd-bootx64.efi";
-          "/loader/loader.conf".source = pkgs.writeText "loader.conf" ''
-            timeout 3
-            default nixos.conf
-          '';
-        };
-        repartConfig = {
-          Type = "esp";
-          Format = "vfat";
-          Label = "ESP";
-          SizeMinBytes = "512M";
-        };
-      };
-      "root" = {
-        storePaths = [ config.system.build.toplevel ];
-        repartConfig = {
-          Type = "root";
-          Format = "ext4";
-          Label = "nixos";
-          Minimize = "guess";
-        };
-      };
-    };
-  };
 }
