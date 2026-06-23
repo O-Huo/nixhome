0c3f41cecabb77706005933071bf9f0ab2d352e9
aoli-al <aoli.al@hotmail.com>
2026-06-23 11:23:09 -0500

    add thunderbird conversation

--- programs/gui-apps.nix
+++ programs/gui-apps.nix
@@ -3,7 +3,19 @@
 # they get proper /Applications/Nix Apps aliases that Spotlight/Launchpad index;
 # on Linux they stay in the home-manager profile.
 pkgs: with pkgs; [
-  thunderbird
+  # Thunderbird with the Conversations add-on installed declaratively via the
+  # enterprise ExtensionSettings policy (the same mechanism the Firefox wrapper
+  # uses). "normal_installed" pins the add-on so users can't remove it.
+  (thunderbird.override {
+    extraPolicies = {
+      ExtensionSettings = {
+        "gconversation@xulforum.org" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.thunderbird.net/thunderbird/downloads/latest/gmail-conversation-view/latest.xpi";
+        };
+      };
+    };
+  })
   google-chrome
   firefox
   qtpass
