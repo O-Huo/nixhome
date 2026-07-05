d8b272f285d6964d59180a2b350b36752ec7e843
aoli-al <aoli.al@hotmail.com>
2026-07-05 18:43:20 -0400

    update

--- hosts/octal/default.nix
+++ hosts/octal/default.nix
@@ -10,4 +10,11 @@
 
   boot.kernel.sysctl."kernel.perf_event_paranoid" = 1;
   networking.hostName = "octal";
+
+  # Wake from suspend only via keyboard: disable USB wakeup for the
+  # Razer mouse (1532), keep it enabled for Keychron keyboards (3434).
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="1532", ATTR{power/wakeup}="disabled"
+    ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="3434", ATTR{power/wakeup}="enabled"
+  '';
 }
