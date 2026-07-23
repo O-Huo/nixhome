f4328ae63bc8cc3725ebd0c860f8c4639b7ee647
aoli-al <aoli.al@hotmail.com>
2026-07-23 15:35:24 -0700

    ruby: enable powertop auto-tune and PCIe ASPM powersupersave

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -80,6 +80,9 @@
   ];
   services.udev.packages = [ pkgs.brightnessctl ];
 
+  powerManagement.powertop.enable = true;
+  boot.kernelParams = [ "pcie_aspm.policy=powersupersave" ];
+
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
   services.logind.settings.Login.HandleLidSwitch = "suspend";
