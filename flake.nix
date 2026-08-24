afc4cf59513955ff19b63d5d01013483ee75293b
aoli-al <aoli.al@hotmail.com>
2026-08-24 12:53:34 -0400

    update cache config

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://chaotic-nyx.cachix.org"
+      "https://cache.numtide.com"
     ];
     extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      # https://cache.numtide.com/index.html
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
   inputs = {
--- programs/hosts/arrakis.nix
+++ programs/hosts/arrakis.nix
@@ -2,6 +2,17 @@
 {
   targets.genericLinux.enable = true;
 
+  # Ubuntu box: there is no NixOS module to carry these, and nix itself comes
+  # from the system daemon profile, so write ~/.config/nix/nix.conf directly
+  # rather than via `nix.settings` (which would pull in its own nix package).
+  # The substituters below are only honoured because `aoli` is listed in
+  # trusted-users in /etc/nix/nix.conf.
+  home.file.".config/nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+    extra-substituters = https://cache.numtide.com https://nix-community.cachix.org
+    extra-trusted-public-keys = niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+  '';
+
   home.packages = [
     pkgs.google-cloud-sdk
   ];
