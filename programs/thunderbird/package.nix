0078b9234de21dde2f3e0b3c7fcf293d40720ea5
aoli-al <aoli.al@hotmail.com>
2026-09-13 17:40:16 -0700

    improve thunderbird config

--- programs/thunderbird/package.nix
+++ programs/thunderbird/package.nix
@@ -8,6 +8,30 @@ pkgs.thunderbird.override {
       };
     };
     Preferences = {
+      "mailnews.mark_message_read.auto" = {
+        Value = true;
+        Status = "user";
+      };
+      "mailnews.mark_message_read.delay" = {
+        Value = false; # mark as read immediately when displayed
+        Status = "user";
+      };
+      "mailnews.start_page.enabled" = {
+        Value = false;
+        Status = "user";
+      };
+      "mail.pane_config.dynamic" = {
+        Value = 1; # wide layout: message below both folder and message lists
+        Status = "user";
+      };
+      "mail.threadpane.listview" = {
+        Value = 0; # cards keep the message list readable in narrow windows
+        Status = "user";
+      };
+      "mail.threadpane.cardsview.rowcount" = {
+        Value = 2;
+        Status = "user";
+      };
       "mailnews.default_sort_type" = {
         Value = 18; # by date
         Status = "user";
